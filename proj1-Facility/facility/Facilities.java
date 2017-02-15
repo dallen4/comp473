5f4d5b20de2bea69cbff8fb61e9991e73f218acf
Nieky Allen <dallen4@luc.edu>
2017-02-14 18:24:31 -0600

    minor progress/tweaks

---
+++ proj1-Facility/facility/Facilities.java
@@ -0,0 +1,11 @@
+package facility;
+
+import facility.Facility;
+
+public class Facilities<Facility> {
+
+	public Facilities() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ proj1-Facility/facility/Facility.java
@@ -0,0 +1,16 @@
+package facility;
+
+import java.util.List;
+import java.util.Vector;
+import facility.Detail;
+
+public class Facility {
+	
+	private List<String> Details = new Vector<String>();
+	
+	public Facility() {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
